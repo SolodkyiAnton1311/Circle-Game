aa582c6554699766530b4382d0acfb3c518152c6
SolodkyiAnton1311 <solodkyi.anton1311@gmail.com>
2024-12-23 13:22:16 +0100

    HotFix

--- Assets/Scripts/Square/Square.cs
+++ Assets/Scripts/Square/Square.cs
@@ -6,13 +6,15 @@ public class Square : MonoBehaviour
     private UIController _uiController;
     [Inject]
     private SquareSpawner _spawner;
+    [Inject]
+    private SquarePool squarePool;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             _spawner.GenerateSquare();
             _uiController.scoreInvoker.Value++;
-            Destroy(gameObject);
+            squarePool.Despawn(this);
         }
     }
 }
